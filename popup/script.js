7cdd2569491651950861d10106153bc2f1106d5f
asdf-a11 <williamdavidchapman@gmail.com>
2025-07-20 17:18:27 +0100

    ImagesAsButtons

--- popup/script.js
+++ popup/script.js
@@ -9,7 +9,78 @@ const MAX_NUMBER_OF_FILES = 3;
 var prevFileNames = undefined;
 var hasLoadedFile = false;
 
-import * as menuModual from "./menus.js"
+const languageList = [
+    {name: "russian", imgPath: "russianFlag.png", freeTranslationPath: "RUtoEN_free.txt", paidTranslationPath:null}
+];
+
+function SelectALanguage(languageData){
+    console.log("Selected a language ", languageData);
+}
+function DisplaySelectLanguageMenu(){  
+    //Display languages buttons to user to click on and select a language
+    const imageRelPath = "../assets/Flags/";
+    //Clear language display div
+    let languageListDiv = document.getElementById("languageListDiv");
+    languageListDiv.innerHTML = "";
+    //Display languages to the screen
+    for(let languageData of languageList){
+        let buttonElement = document.createElement("button");
+        let imgPath = imageRelPath + languageData.imgPath;
+        let imgElement = document.createElement("img");
+        imgElement.src = imgPath;
+        imgElement.className = "languageImage";
+        buttonElement.appendChild(imgElement);
+        buttonElement.onclick = function(){SelectALanguage(languageData);}
+        languageListDiv.appendChild(buttonElement);
+    }  
+}
+
+const menuList = {
+    SelectLanguage: {id:"SelectLanguage",displayFunc:DisplaySelectLanguageMenu},
+    OperationMenu: {id:"OperationsMenu",displayFunc:null}, 
+    SettingsMenu: {id:"SettingsMenu",displayFunc:null},
+    PrevMenu: {id:"PrevMenu",displayFunc:null} // uses prevMenu to go back to menu before prior
+}
+let currentMenuId = undefined;
+let prevMenuId = undefined;
+
+function HideAllMenues(){
+    for(let key in menuList){
+        let m = menuList[key];
+        if(m.id == "PrevMenu"){continue;}
+        document.getElementById(m.id).hidden = true;
+    }
+}
+function GetMenuDataById(idString){
+    for(let key in menuList){
+        let m = menuList[key];
+        if(m.id === idString){
+            return m;
+        }
+    }
+    console.error("Failed to find menu with id -> ", idString);
+    return undefined;
+}
+function ChangeMenu(newMenuId){
+    console.log("Changing menues from ", currentMenuId, " to ", newMenuId);
+    HideAllMenues();
+    //Check if backtrack menues
+    if(newMenuId === menuList.PrevMenu.id){
+        currentMenuId = prevMenuId;
+        prevMenuId = null;
+    }
+    else{
+        prevMenuId = currentMenuId;
+        currentMenuId = newMenuId;
+    }    
+    let menuData = GetMenuDataById(currentMenuId);
+    //Make the menu visable
+    document.getElementById(menuData.id).hidden = false;
+    //If there is a display function for the menu call it
+    if(menuData.displayFunc != null){
+        menuData.displayFunc();
+    }
+}
 
 function SendMessageToBackground(message, func) {
     function ResponseFunction(response) {
@@ -57,16 +128,6 @@ function LoadPrevoiseFileNames(){
         } 
     });
 }    
-
-function ChangeMenu(newMenu){
-    HideAllMenues();
-    //backtrack menues
-    if(newMenu === menuEnum.PrevMenu){
-        currentMenu = prevMenu;
-
-        return;
-    }
-}
 
 function OnSettingsButtonClicked(){
     HideAllMenues();
@@ -126,7 +187,7 @@ function OnStatButtonPress(){
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOing something");
-    menuModual.ChangeMenu(menuModual.menuList.SelectLanguage.id);
+    ChangeMenu(menuList.SelectLanguage.id);
     //LoadPrevoiseFileNames();
     //document.getElementById("pickedFileName_button").onclick = OnSelectedTranslationFile;
     document.getElementById("WordifyWholePage_button").onclick = OnWordifyWholePage;
